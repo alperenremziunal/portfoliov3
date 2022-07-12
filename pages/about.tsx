22e5f21c12c89a05b9b802ce049434918d2da02c
Alperen Remzi Ünal <alperen061706@gmail.com>
2022-07-12 16:41:25 +0300

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,7 +7,7 @@ import avatarBW from 'public/avatar-bw.jpg';
 
 export default function About() {
   return (
-    <Container title="About – Lee Robinson">
+    <Container title="About – Alperen Remzi Ünal">
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           About Me
@@ -16,67 +16,59 @@ export default function About() {
           <h2>Links</h2>
           <ul>
             <li>
-              Twitter: <a href="https://twitter.com/leeerob">@leeerob</a>
+              Twitter: <a href="https://twitter.com/alprnunal">@alprnunal</a>
             </li>
             <li>
-              GitHub: <a href="https://github.com/leerob">@leerob</a>
+              GitHub: <a href="https://github.com/alperenremziunal">@alperenremziunal</a>
             </li>
             <li>
               Website:{' '}
-              <Link href="https://leerob.io">
-                <a>https://leerob.io</a>
+              <Link href="https://alperenremziunal.com">
+                <a>https://alperenremziunal.com</a>
               </Link>
             </li>
             <li>
               LinkedIn:{' '}
-              <a href="https://www.linkedin.com/in/leeerob/">
-                https://www.linkedin.com/in/leeerob
+              <a href="https://www.linkedin.com/in/alperenremziunal/">
+                https://www.linkedin.com/in/alperenremziunal
               </a>
             </li>
           </ul>
           <h2>Bio</h2>
           <h3>Job Title</h3>
-          <p>Lee Robinson, VP of Developer Experience at Vercel</p>
+          <p>Alperen Remzi Ünal, Computer Science Student at -</p>
           <h3>Long, 3rd Person</h3>
           <p>
-            Lee Robinson is the VP of Developer Experience at{' '}
-            <a href="http://vercel.com/">Vercel</a>, where he helps developers
-            build a faster web and leads the Next.js community. He leads the
-            community for the React framework Next.js and is an open-source
-            contributor. An educator, writer, and speaker, Lee has created
-            courses on React, Next.js, and web development.
+            Alperen Remzi Ünal is Computer Science Student at{' '}
+            <a href="https://alperenremziunal.com/">-</a>, ----------
           </p>
           <h3>Long, 1st Person</h3>
           <p>
-            Hey, I'm Lee. I'm the VP of Developer Experience at{' '}
-            <a href="https://vercel.com/">Vercel</a>, where my team helps
-            developers build a faster web. I'm a Next.js contributor and help
-            lead our open-source communities. I've passionate about frontend
-            development and have created courses on React, Next.js, and web
-            development. I'm an advisor and investor in early stage startups.
+            Hey, I'm Alperen. I'm Computer Science Student at{' '}
+            <a href="https://alperenremziunal.com/">-</a>, -------
           </p>
           <h3>Short, 3rd Person</h3>
           <p>
-            Lee Robinson is the VP of Developer Experience at{' '}
-            <a href="http://vercel.com/">Vercel</a>, where he helps developers
+            Alperen Remzi Ünal is Computer Science Student at{' '}
+            <a href="https://alperenremziunal.com/">-</a>, where he helps developers
             build a faster web.
           </p>
           <h3>Short, 1st Person</h3>
           <p>
-            Hey, I'm Lee. I'm the VP of Developer Experience at{' '}
-            <a href="https://vercel.com/">Vercel</a>, where my team helps
+            Hey, I'm Alperen. I'm Computer Science Student at{' '}
+            <a href="https://alperenremziunal.com/">-</a>, where my team helps
             developers build a faster web.
           </p>
           <h3>Education</h3>
           <p>
-            Lee Robinson graduated from Iowa State University with a B.S. in
+            Alperen Remzi Unal graduated from - University with a B.S. in
             Computer Engineering.
           </p>
           <h2>Headshots</h2>
           <div className="flex space-x-8">
             <a href="/avatar.jpg">
               <Image
-                alt="Lee Robinson headshot"
+                alt="Alperen Remzi Ünal headshot"
                 width={400}
                 quality={100}
                 src={avatar}
@@ -85,7 +77,7 @@ export default function About() {
             </a>
             <a href="/avatar-bw.jpg">
               <Image
-                alt="Lee Robinson headshot"
+                alt="Alperen Remzi Ünal headshot"
                 width={400}
                 quality={100}
                 src={avatarBW}
