638ad077374da5c22655f0d660d7388f06f9bbc3
Alperen Remzi Ünal <alperen061706@gmail.com>
2022-07-12 16:34:53 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,20 +15,19 @@ export default function Home() {
           <div className="flex flex-col-reverse sm:flex-row items-start">
             <div className="flex flex-col pr-8">
               <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white">
-                Lee Robinson
+                Alperen Remzi Ünal
               </h1>
               <h2 className="text-gray-700 dark:text-gray-200 mb-4">
-                VP of Developer Experience at{' '}
-                <span className="font-semibold">Vercel</span>
+                Computer Science Student at{' '}
+                <span className="font-semibold">-</span>
               </h2>
               <p className="text-gray-600 dark:text-gray-400 mb-16">
-                Helping developers build a faster web. Teaching about web
-                development, serverless, and React / Next.js.
+                A computer science student who can think outside of the box, is results-oriented, outspoken and analytical.
               </p>
             </div>
             <div className="w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 mr-auto">
               <Image
-                alt="Lee Robinson"
+                alt="Alperen Remzi Ünal"
                 height={176}
                 width={176}
                 src="/avatar.jpg"
@@ -44,17 +43,17 @@ export default function Home() {
           </h3>
           <div className="flex gap-6 flex-col md:flex-row">
             <BlogPostCard
-              title="Everything I Know About Style Guides, Design Systems, and Component Libraries"
+              title="-s"
               slug="style-guides-component-libraries-design-systems"
               gradient="from-[#D8B4FE] to-[#818CF8]"
             />
             <BlogPostCard
-              title="Rust Is The Future of JavaScript Infrastructure"
+              title="-"
               slug="rust"
               gradient="from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]"
             />
             <BlogPostCard
-              title="Past, Present, and Future of React State Management"
+              title="-"
               slug="react-state-management"
               gradient="from-[#FDE68A] via-[#FCA5A5] to-[#FECACA]"
             />
@@ -80,41 +79,39 @@ export default function Home() {
           </Link>
 
           <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-16 text-black dark:text-white">
-            Learn React & Next.js
+            -
           </h3>
           <p className="text-gray-600 dark:text-gray-400 mb-4">
-            Build and deploy a modern SaaS application using the most popular
-            open-source software. This course is 12 hours long and is completely
-            live streamed.
+            -
           </p>
           <VideoCard
             index="01"
-            href="https://www.youtube.com/watch?v=MxR5I5_hOKk&list=PL6bwFJ82M6FXgctyoWXqj7H0GK8_YIeF1&index=2"
+            href="https://www.youtube.com/watch"
             length="1:02:45"
-            title="Introduction to React 2025"
+            title="-"
           />
           <VideoCard
             index="02"
-            href="https://www.youtube.com/watch?v=AGl52moyISU&list=PL6bwFJ82M6FXgctyoWXqj7H0GK8_YIeF1&index=3"
+            href="https://www.youtube.com/watch"
             length="54:22"
-            title="Firestore, Chakra UI, Absolute Imports"
+            title="-"
           />
           <VideoCard
             index="03"
-            href="https://www.youtube.com/watch?v=3g6-v3_BNbM&list=PL6bwFJ82M6FXgctyoWXqj7H0GK8_YIeF1&index=4"
+            href="https://www.youtube.com/watch"
             length="1:08:30"
-            title="Designing & Building the Dashboard"
+            title="-"
           />
           <VideoCard
             index="04"
-            href="https://www.youtube.com/watch?v=u8iv_yhSRI8&list=PL6bwFJ82M6FXgctyoWXqj7H0GK8_YIeF1&index=5"
+            href="https://www.youtube.com/watch"
             length="1:13:45"
-            title="Firebase Admin with Next.js + SWR"
+            title="-"
           />
           <a
             target="_blank"
             rel="noopener noreferrer"
-            href="https://www.youtube.com/playlist?list=PL6bwFJ82M6FXgctyoWXqj7H0GK8_YIeF1"
+            href="https://www.youtube.com/playlist"
             className="flex mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6"
           >
             Watch all videos
